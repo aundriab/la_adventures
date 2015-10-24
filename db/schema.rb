938af044592133a55f4fd9143aae871b070c378b
aundriab <aundriab@yahoo.com>
2015-10-24 11:36:41 -0700

    Generated adventure scaffold; ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151024182213) do
+
+  create_table "adventures", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "picture"
+    t.date     "visit"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
